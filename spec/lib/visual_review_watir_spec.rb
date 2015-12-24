cb755df9f2a82cbb69342c62588ea3be0256283d
Gustavo Kath <gustavokath@MacBook-Pro-de-Gustavo.local>
2015-12-24 16:09:04 -0200

    Unit tests for #start_run method

--- spec/lib/visual_review_watir_spec.rb
+++ spec/lib/visual_review_watir_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 module Lib
   describe VisualReviewWatir do
     let(:visual_review){ VisualReviewWatir.new }
+    let(:post_runs_response_body){"{\"id\":3,\"suiteId\":1,\"branchName\":\"master\",\"baselineTreeId\":1,\"startTime\":\"2015-12-24T15:41:48-0200\",\"endTime\":null,\"status\":\"running\",\"projectId\":1}"}
     describe '.initialize' do
       context 'instalize component' do
         it 'with default values' do
@@ -40,30 +41,34 @@ module Lib
     end
 
     describe '#start_run' do
-      context 'when run is started' do
-        it 'returns code 201' do
-          response = double(Net::HTTPCreated, code: "201")
-          allow(VisualReviewWatir).to receive(:server_active?).and_return(true)
-
-
+      context 'when api version is compatible' do
+        let(:valid_response){double(Net::HTTPCreated, code: "201", body: post_runs_response_body)}
+        let(:valid_api){double(Net::HTTPOK, code: "200", body: "1")}
+        before(:all) do
+          allow(Net::HTTP).to receive(:request).and_return(valid_response)
+          allow(VisualReviewWatir).to receive(:checkApiVersion).and_return(valid_api)
         end
 
-        it 'returns json' do
-          allow(VisualReviewWatir).to receive(:server_active?).and_return(true)
-          debugger
-          result = visual_review.start_run("Project 0", "Suite 1")
-          expect(result).to
+        it 'returns run id info' do
+          response = visual_review.start_run("Project0", "Suite1")
+          expect(response.has_key?("run_id"))
         end
 
-        it 'returns run id' do
-          allow(VisualReviewWatir).to receive(:server_active?).and_return(true)
-          result = visual_review.start_run("Project 0", "Suite 1")
+        it 'returns run date info' do
+          response = visual_review.start_run("Project0", "Suite1")
+          expect(response.has_key?("run_date"))
+        end
+
+        it 'returns hash' do
+          response = visual_review.start_run("Project0", "Suite1")
+          expect(response).to be_a(Hash)
         end
       end
-
-      context 'when server is not active' do
-        it 'ruturns error message' do
-          allow(VisualReviewWatir).to receive(:server_active?).and_return(false)
+      context 'when api version is not compatible' do
+        let(:invalid_api){double(Net::HTTPOK, code: "200", body: "50")}
+        it 'returns runtimeError exeception' do
+          allow(VisualReviewWatir).to receive(:checkApiVersion).and_return(invalid_api)
+          expect{visual_review.start_run("Project0", "Suite1")}.to raise_error
         end
       end
     end
